a3912b4f971e9b6152d7cfad4ff7d0fbaafdb32a
System Administrator <admin@example.com>
2025-11-05 12:12:25 +0530

    1.3

---
+++ src/fluent/generated/sys_scope_privilege_f2f73f85c34d72104963991ed40131ad.now.ts
@@ -0,0 +1,10 @@
+import { CrossScopePrivilege } from '@servicenow/sdk/core'
+
+CrossScopePrivilege({
+    $id: Now.ID['f2f73f85c34d72104963991ed40131ad'],
+    operation: 'execute',
+    status: 'allowed',
+    targetName: 'GlideRecordSecure.getValue',
+    targetScope: 'global',
+    targetType: 'scriptable',
+})
